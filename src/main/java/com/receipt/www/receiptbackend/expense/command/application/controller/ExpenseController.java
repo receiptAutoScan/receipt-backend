5a2475b6910361d50db5a12b639836460efa9d17
ej20001000 <ejayyj1225@gmail.com>
2023-09-11 18:57:45 +0900

    Feat: 영수증 이미지 프로세싱 로직 구현 HttpRequest를 어떻게 보낼지 찾아봐야됨
    Fixes: #15

--- src/main/java/com/receipt/www/receiptbackend/expense/command/application/controller/ExpenseController.java
+++ src/main/java/com/receipt/www/receiptbackend/expense/command/application/controller/ExpenseController.java
@@ -3,9 +3,24 @@ package com.receipt.www.receiptbackend.expense.command.application.controller;
 import com.receipt.www.receiptbackend.expense.command.application.dto.CreateExpenseDTO;
 import com.receipt.www.receiptbackend.expense.command.application.dto.UpdateExpenseDTO;
 import com.receipt.www.receiptbackend.expense.command.application.service.ExpenseService;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 import org.springframework.web.bind.annotation.*;
 
+import java.awt.*;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.http.HttpClient;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class ExpenseController {
@@ -16,6 +31,11 @@ public class ExpenseController {
     }
 
     @PostMapping("/expense")
+    public void processExpenseReceipt(@RequestBody List<Image> imageList) {
+
+    }
+
+    @PostMapping("/expense/json")
     public void createExpenseList(@RequestBody List<CreateExpenseDTO> createExpenseDTOList) {
 
         expenseService.createExpenseList(createExpenseDTOList);
